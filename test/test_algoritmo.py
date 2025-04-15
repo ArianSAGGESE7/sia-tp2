0b64f6d674125ae082bd1c3252d211fee02193e8
ArianSAGGESE7 <ariansaggese@alu.ing.unlp.edu.ar>
2025-04-15 08:46:35 -0300

    Archivos p video, resultados y test

--- test/test_algoritmo.py
+++ test/test_algoritmo.py
@@ -2,76 +2,27 @@ import cv2
 from src.mainp2 import genetic_algorithm
 from src.individuo import crear_imagen, crear_individuo
 
+img = cv2.imread("Images/ucrania.jpg")
+img = cv2.cvtColor(img, cv2.COLOR_RGB2RGBA)
+img = cv2.resize(img, (100, 100)) 
 
-def test_00():
-    "ejemplo de uso"
-    img = cv2.imread("Images/image_00.png")
-    img = cv2.cvtColor(img, cv2.COLOR_RGB2RGBA)
-    img = cv2.resize(img, (100, 100)) 
-
-
-    opciones = {
-        "num_poligonos": 1,
-        "num_lados": 3,
-        "num_individuos": 100,
-        "num_generaciones": 200,
-        "mutacion": {
-            "prob_mutacion": 0.1,
-            "cant_mutacion": 0.1
-            },
-        "cruza": {
-            "metodo": "cruce_un_punto"
-            },
-        "seleccion": {
-            "num_seleccion_elite": 3,
-            "num_nuevos_individuos": 5,
-            "metodo": "random",
-            "K": 5,
-            "T": 1,
-            "dT": 0.1,
-            "num_ind_torneo": 5,
-            "threshold_torneo": 0.5},
-        "paso_para_resultados_detallados": 5
-    }
-
-    metricas = genetic_algorithm(img, opciones)
-    assert isinstance(metricas, dict)
+opciones = {
+    "num_poligonos": 25,
+    "num_lados": 3,
+    "num_individuos": 50,
+    "num_generaciones": 40000,
+    "num_seleccion_elite": 25,
+    "prob_mutacion": 0.9,
+    "cant_mutacion": 0.1,
+    "metodo_de_cruza": "cruce_uniforme",
+    "kwargs_cruza": {},
+    "metodo_de_seleccion": "elite",
+    "kwargs_seleccion": {"K": 5, "T":10, "dT":0.001, "num_ind_torneo": 5, "threshold_torneo": 0.5},
+}
 
 
-def test_01():
-    "ejemplo de uso"
+mejor,fit = genetic_algorithm(img, opciones)
+mejor_img = crear_imagen(mejor)
+cv2.imwrite("out.jpg", mejor_img)
+cv2.imwrite("target.jpg", img)
 
-    img = cv2.imread("Images/blacksquare.jpg")
-    img = cv2.cvtColor(img, cv2.COLOR_RGB2RGBA)
-    img = cv2.resize(img, (100, 100)) 
-
-    opciones = {
-        "num_poligonos": 2,
-        "num_lados": 4,
-        "num_individuos": 100,
-        "num_generaciones": 200,
-        "mutacion": {
-            "prob_mutacion": 0.1,
-            "cant_mutacion": 0.1
-            },
-        "cruza": {
-            "metodo": "cruce_un_punto"
-            },
-        "seleccion": {
-            "num_seleccion_elite": 3,
-            "num_nuevos_individuos": 5,
-            "metodo": "random",
-            "K": 5,
-            "T": 1,
-            "dT": 0.1,
-            "num_ind_torneo": 5,
-            "threshold_torneo": 0.5},
-        "paso_para_resultados_detallados": 5
-    }
-
-    metricas = genetic_algorithm(img, opciones)
-
-    mejor = metricas["mejor"]
-    img = crear_imagen(mejor)
-    # cv2.imwrite("out.jpg", img)
-    assert isinstance(metricas, dict)
